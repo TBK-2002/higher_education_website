e81a7d9f9d2eed16eb717fb60e7969a8af728522
Nour El-din <nour.sehs.3@gmail.com>
2023-04-10 04:49:34 +0200

    Added ajax to load courses

---
+++ course.js
@@ -0,0 +1,10 @@
+class Course{
+    constructor(name, id, department, hours, day, hall){
+        this.name = name;
+        this.id = id;
+        this.department = department;
+        this.hours = hours;
+        this.day = day;
+        this.hall = hall;
+    }
+}
