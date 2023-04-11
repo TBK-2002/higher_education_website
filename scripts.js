06e484fcd7b356bccfd77edf6d9c88b37b4fe171
Ahmed yehia <yehiaahmed900@gmail.com>
2023-04-11 13:37:58 +0200

    added validation to edit student page and added students.json

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 class Student {
-  constructor(university, department, status, courses) {
-    this.university = university;
+  constructor(id,name, department, status, courses) {
+    this.id = id;
+    this.name = name;
     this.department = department;
     this.status = status;
     this.courses = courses;
@@ -17,14 +18,38 @@ class Student {
   
     const dept = document.getElementById('dept');
     const status = document.getElementById('status');
-    const course1 = doccument.getElementById('course1');
+    const course1 = document.getElementById('course1');
     const course2 = document.getElementById('course2');
     const course3 = document.getElementById('course3');
 
-    if(dept == 0 || status == 0 || course1 == 0 || course2 == 0 || course3 == 0){
+    if(dept.value == 0 || status.value == 0 || course1.value == 0 || course2.value == 0 || course3.value == 0){
       window.alert('Please fill all the fields');
-      return false;
+
     }
-  
-    return true;
+    else if(dept.value == 1 && (course1.value != 6 && course1.value!= 7) || (course2.value != 6 && course2.value!= 7) || (course3.value != 6 && course3.value!= 7)){
+      window.alert('Please select a course from the list of courses offered by the department');
+    }
+    else if(dept.value == 2 && (course1.value !=1 && course1.value!=4 && course1.value!=5) || (course2.value !=1 && course2.value!=4 && course2.value!=5) || (course3.value !=1 && course3.value!=4 && course3.value!=5)){
+      window.alert('Please select a course from the list of courses offered by the department');
+    }
+    else if(dept.value == 3 && (course1.value !=3 && course1.value!=4 && course1.value!=7) || (course2.value !=3 && course2.value!=4 && course2.value!=7) || (course3.value !=3 && course3.value!=4 && course3.value!=7)){
+      window.alert('Please select a course from the list of courses offered by the department');
+    }
+    else if(dept.value == 4 && (course1.value !=2 && course1.value!=4) || (course2.value !=2 && course2.value!=4) || (course3.value !=2 && course3.value!=4 )){
+      window.alert('Please select a course from the list of courses offered by the department');
+    }
+    else if(dept.value == 5 && (course1.value !=5) || (course2.value!=5) ||  (course3.value !=5)){
+      window.alert('Please select a course from the list of courses offered by the department');
+    }
+
+    else{
+      window.alert('Form submitted successfully');
+      if(status.value == 1){
+        window.location.href = 'students.html';
+      }
+      else{
+        window.location.href = 'inactive_students.html';
+      }
+    }
   }
+
