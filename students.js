6d2bc178862696ab598c4e5060ef6c53d444ef81
Ahmed yehia <yehiaahmed900@gmail.com>
2023-04-12 12:06:42 +0200

    added students.json to local storage and retrieved the data from it in students.html

---
+++ students.js
@@ -0,0 +1,9 @@
+class Student {
+    constructor(id,name, department, status, courses) {
+      this.id = id;
+      this.name = name;
+      this.department = department;
+      this.status = status;
+      this.courses = courses;
+    }
+  }
