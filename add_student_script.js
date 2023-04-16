fba1cc7d7dc13cad713af40dd3a2fe005052a527
Ahmed yehia <yehiaahmed900@gmail.com>
2023-04-16 19:40:04 +0200

    fixed some bugs

--- add_student_script.js
+++ add_student_script.js
@@ -93,7 +93,7 @@ function display_inactive_students(){
         for(var i in loadedStudents){
             if(loadedStudents[i].stat == "Active")
                 continue;
-      if((std_lname == null && std_lname == null)||(loadedStudents[i].fname.toLowerCase().includes(std_fname) || loadedStudents[i].lname.toLowerCase().includes(std_lname)) ||  (loadedStudents[i].fname.toLowerCase() == std_fname && loadStudents[i].lname.toLowerCase() == std_lname)){        
+      if((std_fname == null && std_lname == null)||(loadedStudents[i].fname.toLowerCase().includes(std_fname) || loadedStudents[i].lname.toLowerCase().includes(std_lname)) ||  (loadedStudents[i].fname.toLowerCase() == std_fname && loadStudents[i].lname.toLowerCase() == std_lname)){        
        if((document.getElementById("inactive_filter").value == loadedStudents[i].department || document.getElementById("inactive_filter").value == 0)){  
             content+="<td>";
             content+="<div class = 'inactive_card'>";
