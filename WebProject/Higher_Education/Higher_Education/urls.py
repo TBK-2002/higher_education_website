822833ac59f7b7d66467314139b091cd90744523
Nour El-Din Ahmed Hussein <nour.sehs.3@gmail.com>
2023-05-18 22:06:23 +0300

    7asby allah w ne3m el wakel

--- WebProject/Higher_Education/Higher_Education/urls.py
+++ WebProject/Higher_Education/Higher_Education/urls.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('', include('home_page.urls')),
+    path('', include('mainApp.urls')),
     path('admin/', admin.site.urls),
-
 ]
--- WebProject/Higher_Education/mainApp/views.py
+++ WebProject/Higher_Education/mainApp/views.py
@@ -1,7 +1,5 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-
 
 def home_page(request):
   template = loader.get_template('home_page.html')
