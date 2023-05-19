15a599de3333126b6f97d66d5c8ed78a8e86795b
Nour El-Din Ahmed Hussein <nour.sehs.3@gmail.com>
2023-05-19 20:25:37 +0300

    brrrrrr

--- WebProject/Higher_Education/mainApp/models.py
+++ WebProject/Higher_Education/mainApp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Course(models.Model):
+    course_name = models.CharField(max_length=200)
+    course_id = models.IntegerField(default=0)
+    course_department = models.CharField(max_length=200)
+    course_hours = models.IntegerField(default=0)
+    course_day = models.CharField(max_length=200)
+    course_hall = models.IntegerField(default=0)
+    course_students_count = models.IntegerField(default=0)
+    def __str__(self):
+        return self.course_name
+    
--- WebProject/Higher_Education/mainApp/views.py
+++ WebProject/Higher_Education/mainApp/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.template import loader
+from .models import Course
 
 def home_page(request):
   template = loader.get_template('home_page.html')
@@ -7,4 +8,16 @@ def home_page(request):
 
 def main_page(requeset):
   template = loader.get_template('main_page.html')
+  return HttpResponse(template.render())
+
+def courses_page(requeset):
+  courses = Course.objects.all().values()
+  template = loader.get_template('courses.html')
+  context = {
+    'courses': courses,
+  }
+  return HttpResponse(template.render(context, requeset))
+
+def add_course_page(request):
+  template = loader.get_template('add_new_course.html')
   return HttpResponse(template.render())
