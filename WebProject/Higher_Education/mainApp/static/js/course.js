2fec13d17c84ddec427c771c23aa0c79c357fc4f
Ahmed yehia <yehiaahmed900@gmail.com>
2023-05-19 17:25:41 +0200

    edited

---
+++ WebProject/Higher_Education/mainApp/static/js/course.js
@@ -0,0 +1,11 @@
+class Course{
+    constructor(name, id, department, hours, day, hall){
+        this.name = name;
+        this.id = id;
+        this.department = department;
+        this.hours = hours;
+        this.day = day;
+        this.hall = hall;
+        this.studentsCount = 0;
+    }
+}
