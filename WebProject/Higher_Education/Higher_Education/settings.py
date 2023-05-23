0a7b48cceb73f51b766662a63f3d1b01d1edb2d4
Ahmed yehia <yehiaahmed900@gmail.com>
2023-05-23 17:35:31 +0200

    edited allowed hosts

--- WebProject/Higher_Education/Higher_Education/settings.py
+++ WebProject/Higher_Education/Higher_Education/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-c$1p_8=5miokbjur=#-l7j#xa#48(i@kbv+9br%%t(x=7g%xc8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS =  ['localhost', '127.0.0.1']
+ALLOWED_HOSTS  = ['*']
 
 
 # Application definition
